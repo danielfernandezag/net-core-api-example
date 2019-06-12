f0232fbcd7e9258709642dcd2c10df839a60f8b3
Julio Daniel Fernandez Aguirre <danielfernandezaguirre@outlook.com>
2019-06-12 11:01:34 -0500

    First commit

--- GameCollectionAPI/Controllers/UsersController.cs
+++ GameCollectionAPI/Controllers/UsersController.cs
@@ -72,6 +72,12 @@ namespace GameCollectionAPI.Controllers
 
             return Ok(userResource);
         }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAsync(short id)
+        {
+
+        }
     }
 }
 
